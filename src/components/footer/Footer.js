25a4c784b3b3676452b27b45fad4cd84e35ab506
EvanGottschalk <magnus5557@gmail.com>
2024-04-10 19:08:10 -0400

    added live contract address

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -37,7 +37,7 @@ const Footer = () => {
           </a>
         </div>
         <div className='footerTextContainer'>
-          <marquee>Contract Address: "". . . . . . . . . . . . . . . . . . . . Contract Name: "MelioraComicV1". . . . . . . . . . . . . . . . . . . . Contract Symbol: MELIORA1 . . . . . . . . . . . . . . . . . . . .  Contract URI:  . . . . . . . . . . . . . . . . . . . . Collection Name: "The Genesis of Meliora" . . . . . . . . . . . . . . . . . . . . </marquee>
+          <marquee>Contract Address: "0x4E35d70889B5A698fE9AE2C19CB280f29DBDF7eC". . . . . . . . . . . . . . . . . . . . Contract Name: "TheBirthOfMeliora". . . . . . . . . . . . . . . . . . . . Contract Symbol: MELIORA1 . . . . . . . . . . . . . . . . . . . .  Collection URI: https://nftstorage.link/ipfs/bafybeidicmrt6v64jdchhn7jtzz4ydtj3vfmruzyn2t4fxybxhhhyv3lo4/TheGenesisOfMeliora.json . . . . . . . . . . . . . . . . . . . . Collection Name: "The Genesis of Meliora" . . . . . . . . . . . . . . . . . . . . </marquee>
         </div>
       </div>
       <div className='creatorAttributionContainer'>
